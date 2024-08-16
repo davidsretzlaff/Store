dbf36dc2d370ea9173bc185e491f8c9f743b384c
davidsretzlaff <davids.retzlaff@gmail.com>
2024-08-15 23:10:51 -0300

    setup api tests end to end

--- tests/Store.EndToEndTest/Api/User/CreateUser/CreateUserApiTest.cs
+++ tests/Store.EndToEndTest/Api/User/CreateUser/CreateUserApiTest.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Store.Application.UseCases.User.CreateUser.Common;
+using System.Net;
 
 namespace Store.EndToEndTest.Api.User.CreateUser
 {
-	internal class CreateUserApiTest
+	[Collection(nameof(CreateUserApiTestFixture))]
+	public class CreateUserApiTest
 	{
+		private readonly CreateUserApiTestFixture _fixture;
+		public CreateUserApiTest(CreateUserApiTestFixture fixture) => _fixture = fixture;
+
+		[Fact(DisplayName = nameof(CreateCategory))]
+		[Trait("EndToEnd/API", "User/Create - Endpoints")]
+		public async Task CreateCategory()
+		{
+			{
+				var input = _fixture.getExampleInput();
+
+				var (response, output) = await _fixture.ApiClient.Post<ApiResponse<UserOutput>>("/users", input);
+
+				response.Should().NotBeNull();
+				response!.StatusCode.Should().Be(HttpStatusCode.Created);
+				output.Should().NotBeNull();
+				output.Data.Should().NotBeNull();
+				output.Data.Name.Should().Be(input.Name);
+				output.Data.Description.Should().Be(input.Description);
+				output.Data.IsActive.Should().Be(input.IsActive);
+				output.Data.Id.Should().NotBeEmpty();
+				output.Data.CreatedAt.Should().NotBeSameDateAs(default);
+
+				var dbCategory = await _fixture.Persistence.GetById(output.Data.Id);
+				dbCategory.Should().NotBeNull();
+				dbCategory!.Name.Should().Be(input.Name);
+				dbCategory.Description.Should().Be(input.Description);
+			}
+		}
+
 	}
 }
---
+++ tests/Store.EndToEndTest/Base/CustomWebApplicationFactory.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Store.Infra.Data.EF;
+
+namespace Store.EndToEndTest.Base
+{
+	public class CustomWebApplicationFactory<TStartup>
+	   : WebApplicationFactory<TStartup>
+	   where TStartup : class
+	{
+		protected override void ConfigureWebHost(
+			IWebHostBuilder builder
+		)
+		{
+			builder.UseEnvironment("EndToEndTest");
+			builder.ConfigureServices(services =>
+			{
+				var dbOptions = services.FirstOrDefault(
+					x => x.ServiceType == typeof(DbContextOptions<StoreDbContext>));
+				if ( dbOptions is not null )
+				{
+					services.Remove( dbOptions );
+				}
+				services.AddDbContext<StoreDbContext>(
+					options => { options.UseInMemoryDatabase("end2end-tests-db"); }
+					);
+
+				//var serviceProvider = services.BuildServiceProvider();
+				//using var scope = serviceProvider.CreateScope();
+				//var context = scope.ServiceProvider
+				//	.GetService<CatalogDbContext>();
+				//ArgumentNullException.ThrowIfNull(context);
+				//context.Database.EnsureDeleted();
+				//context.Database.EnsureCreated();
+			});
+
+			base.ConfigureWebHost(builder);
+		}
+	}
+}
